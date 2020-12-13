68fe2fb11240c4c66d63e45191522b331ccc4216
nb341 <narindrab4@gmail.com>
2020-12-12 23:20:38 -0400

    added files

---
+++ stuff.js
@@ -0,0 +1,79 @@
+// !! IMPORTANT README:
+
+// You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. 
+
+/***********
+INSTRUCTIONS:
+  - Select the project you would 
+    like to complete from the dropdown 
+    menu.
+  - Click the "RUN TESTS" button to
+    run the tests against the blank 
+    pen.
+  - Click the "TESTS" button to see 
+    the individual test cases. 
+    (should all be failing at first)
+  - Start coding! As you fulfill each
+    test case, you will see them go   
+    from red to green.
+  - As you start to build out your 
+    project, when tests are failing, 
+    you should get helpful errors 
+    along the way!
+    ************/
+
+// PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
+
+// Once you have read the above messages, you can delete all comments. 
+
+const url = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+var quotes;
+var colours = [
+  '#16a085',
+  '#27ae60',
+  '#2c3e50',
+  '#f39c12',
+  '#e74c3c',
+  '#9b59b6',
+  '#FB6964',
+  '#342224',
+  '#472E32',
+  '#BDBB99',
+  '#77B1A9',
+  '#73A857'
+];
+function getQuotes() {
+  return $.get({
+    headers: {
+      Accept: 'application/json'
+    },
+    url:
+      'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
+    success: function (jsonQuotes) {
+      if (typeof jsonQuotes === 'string') {
+        quotes = JSON.parse(jsonQuotes);
+       
+      }
+    }
+  });
+}
+
+function getQuote (){
+   let idx = Math.floor(Math.random()*quotes.quotes.length);
+   //quote = quotes[idx];
+   console.log(quotes.quotes[idx]);
+   var text = document.querySelector('#text');
+  text.innerHTML=quotes.quotes[idx].quote;
+  var author = document.querySelector('#author');
+  author.innerHTML=quotes.quotes[idx].author;
+  let bkg = Math.floor(Math.random()*colours.length);
+  document.querySelector('body').style.backgroundColor=colours[bkg];
+  document.querySelector('button').style.backgroundColor=colours[bkg];
+}
+
+$(document).ready(function(){
+  getQuotes().then(()=>{
+      getQuote();
+    });
+  $('#new-quote').on('click', getQuote);
+});
